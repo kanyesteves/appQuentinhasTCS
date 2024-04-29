7253ca4902c3b3985b4231f883c2d82f89baff45
kanyesteves <kany.esteves@gmail.com>
2024-04-29 19:43:56 -0300

    Initial Commit

---
+++ QuentinhasNow1.5/QuentinhasNow1_5App.swift
@@ -0,0 +1,17 @@
+//
+//  QuentinhasNow1_5App.swift
+//  QuentinhasNow1.5
+//
+//  Created by Kanydian Esteves on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct QuentinhasNow1_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
